1d2271b6fd137ded65bd1bae2ca9122dcc99ca65
smartquailstage <smartquail.stage@gmail.com>
2024-11-04 17:33:20 -0500

    update

--- qnd30opendkim/generate_keys.sh
+++ qnd30opendkim/generate_keys.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Directorio donde se crearán las claves DKIM
+KEY_DIR="/etc/opendkim/keys"
 DOMAIN="juansilvaphoto.com"
 SELECTOR="default"
 
-# Crear las claves si no existen
-if [ ! -f "/etc/opendkim/keys/${SELECTOR}.private" ]; then
-    opendkim-genkey -s ${SELECTOR} -d ${DOMAIN} -D /etc/opendkim/keys
-    chown opendkim:opendkim /etc/opendkim/keys/${SELECTOR}.private
-    chmod 600 /etc/opendkim/keys/${SELECTOR}.private
-    echo "Claves DKIM generadas para ${DOMAIN} con selector ${SELECTOR}"
-else
-    echo "Las claves DKIM ya existen para ${DOMAIN} con selector ${SELECTOR}"
-fi
+# Crear el directorio si no existe
+mkdir -p $KEY_DIR
+
+# Generar las claves DKIM
+opendkim-genkey -D $KEY_DIR -d $DOMAIN -s $SELECTOR
+
+# Cambiar permisos
+chown opendkim:opendkim $KEY_DIR/$SELECTOR.private
+chmod 600 $KEY_DIR/$SELECTOR.private
