38855b4512f611830838109aef81f2b747dc7709
smartquailstage <smartquail.stage@gmail.com>
2024-11-04 23:56:05 -0500

    update

--- qnd30opendkim/generate_keys.sh
+++ qnd30opendkim/generate_keys.sh
@@ -6,11 +6,20 @@ DOMAIN="juansilvaphoto.com"
 SELECTOR="default"
 
 # Crear el directorio si no existe
-mkdir -p $KEY_DIR
+mkdir -p "$KEY_DIR"
 
-# Generar las claves DKIM
-opendkim-genkey -D $KEY_DIR -d $DOMAIN -s $SELECTOR
+# Generar las claves DKIM y guardar la salida
+opendkim-genkey -D "$KEY_DIR" -d "$DOMAIN" -s "$SELECTOR"
 
-# Cambiar permisos
-chown opendkim:opendkim $KEY_DIR/${SELECTOR}.private
-chmod 600 $KEY_DIR/${SELECTOR}.private
+# Cambiar permisos de las claves generadas
+chown -R opendkim:opendkim "$KEY_DIR"
+chown opendkim:opendkim "$KEY_DIR/${SELECTOR}.private"
+chmod 600 "$KEY_DIR/${SELECTOR}.private"
+
+# Imprimir las claves generadas, incluyendo la clave pública y privada
+echo "Claves DKIM generadas para el dominio: $DOMAIN con selector: $SELECTOR"
+echo "Clave pública (public key) que se debe añadir al DNS:"
+cat "$KEY_DIR/${SELECTOR}.txt"
+echo ""
+echo "Clave privada (private key) que se debe configurar en el servidor de correo:"
+cat "$KEY_DIR/${SELECTOR}.private"
