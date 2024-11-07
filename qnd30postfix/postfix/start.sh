7b8de51d27fd4b306fc1b7c2581e0581d1269696
smartquailstage <smartquail.stage@gmail.com>
2024-11-06 22:30:57 -0500

    Update start.sh

--- qnd30postfix/postfix/start.sh
+++ qnd30postfix/postfix/start.sh
@@ -1,191 +1,54 @@
 #!/bin/bash
 
-opendkim -f &
+# Define the user and group
+USER="vmail"
+GROUP="vmail"
 
-# Configuración de las credenciales de PostgreSQL
-export PGPASSWORD="smartquaildev1719pass"
-export PGUSER="sqadmindb"
-export POSTFIX_POSTGRES_DB="POSFIXDB"
-export POSTFIX_POSTGRES_USER="sqadmindb"
-export POSTFIX_POSTGRES_HOST="smartquaildb"
+# Define the main directory
+MAIL_DIR="/home/info/Maildir"
 
-function log {
-  echo "$(date) $ME - $@"
-}
+# Define the directory for the specific user
+INFO_DIR="/home/info/Maildir/info@juansilvaphoto.com/tmp"
 
-function addUserInfo {
-  local user_name="info"
-  local user_home="/home/${user_name}"
+# Start Dovecot in the background
+echo "Starting Dovecot..."
+dovecot &
 
-  # Verifica si el usuario ya existe
-  if ! id -u "$user_name" &>/dev/null; then
-    log "Adding user '${user_name}'"
+# Wait for Dovecot to start up by checking if the socket is available
+echo "Waiting for Dovecot to start..."
+until [ -e "/var/spool/postfix/private/auth" ]; do
+  sleep 1
+done
+echo "Dovecot started successfully."
 
-    # Añade el usuario con un directorio home
-    adduser --system --home "$user_home" --no-create-home "$user_name"
+# Create the main mail directory if it doesn't exist
+if [ ! -d "$MAIL_DIR" ]; then
+  echo "Creating directory $MAIL_DIR"
+  mkdir -p "$MAIL_DIR" || { echo "Failed to create $MAIL_DIR"; exit 1; }
+fi
 
-    # Crea el directorio home
-    mkdir -p "$user_home"
+# Adjust ownership and permissions for the main mail directory
+echo "Setting permissions and ownership for $MAIL_DIR"
+chown -R $USER:$GROUP "$MAIL_DIR" && chmod 755 "$MAIL_DIR" || { echo "Failed to set permissions for $MAIL_DIR"; exit 1; }
 
-    # Ajusta los permisos del directorio home
-    chown "$user_name:$user_name" "$user_home"
-    chmod 700 "$user_home"
+# Recursively adjust ownership and permissions for all directories and files under $MAIL_DIR
+echo "Setting permissions and ownership for all directories and files under $MAIL_DIR"
+find "$MAIL_DIR" -type d -exec chown $USER:$GROUP {} \; -exec chmod 755 {} \; || { echo "Failed to set permissions for directories in $MAIL_DIR"; exit 1; }
+find "$MAIL_DIR" -type f -exec chown $USER:$GROUP {} \; -exec chmod 644 {} \; || { echo "Failed to set permissions for files in $MAIL_DIR"; exit 1; }
 
-    # Asegúrate de que el usuario postfix tenga acceso al directorio home del usuario
-    chown -R postfix:postfix "$user_home"
-    chmod -R 700 "$user_home"
+# Check if the specific subdirectory exists and create it if needed
+if [ ! -d "$INFO_DIR" ]; then
+  echo "Creating directory $INFO_DIR"
+  mkdir -p "$INFO_DIR" || { echo "Failed to create $INFO_DIR"; exit 1; }
+fi
 
-    chown -R vmail:vmail "$user_home"
-    chmod -R 700 "$user_home"
-    
-    log "User '${user_name}' added with home directory '${user_home}'"
-  else
-    log "User '${user_name}' already exists"
-  fi
-}
+# Adjust ownership and permissions for the specific directory
+echo "Setting permissions and ownership for $INFO_DIR"
+chown $USER:$GROUP "$INFO_DIR" && chmod 755 "$INFO_DIR" || { echo "Failed to set permissions for $INFO_DIR"; exit 1; }
 
+# Verify the results
+echo "Verification of permissions and ownership:"
+ls -ld "$MAIL_DIR"
+ls -ld "$INFO_DIR"
 
-function createTable {
-  local table_name=$1
-  local table_sql=$2
-
-  log "Creating ${table_name} table in PostgreSQL..."
-
-  # Check if table exists
-  local check_sql="SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_schema = 'public' AND table_name = '${table_name}');"
-  local table_exists=$(psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -t -c "$check_sql")
-
-  if [[ "$table_exists" =~ ^t$ ]]; then
-    log "Table ${table_name} already exists, skipping creation."
-  else
-    psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "$table_sql"
-    if [ $? -eq 0 ]; then
-      log "${table_name} table created successfully."
-    else
-      log "Failed to create ${table_name} table."
-    fi
-  fi
-}
-
-function createVirtualTables {
-  createTable "virtual_domains" "CREATE TABLE IF NOT EXISTS virtual_domains (
-    id SERIAL PRIMARY KEY,
-    domain VARCHAR(255) NOT NULL UNIQUE
-  );"
-
-  createTable "virtual_aliases" "CREATE TABLE IF NOT EXISTS virtual_aliases (
-    id SERIAL PRIMARY KEY,
-    domain_id INT NOT NULL,
-    source VARCHAR(255) NOT NULL,
-    destination VARCHAR(255) NOT NULL,
-    FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
-  );"
-
-  createTable "virtual_users" "CREATE TABLE IF NOT EXISTS virtual_users (
-    id SERIAL PRIMARY KEY,
-    domain_id INT NOT NULL,
-    password VARCHAR(106) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE
-  );"
-}
-
-function insertInitialData {
-  log "Inserting initial data into PostgreSQL tables..."
-
-  local insert_sql="
-    INSERT INTO virtual_domains (domain) VALUES ('juansilvaphoto.com') ON CONFLICT DO NOTHING;
-    INSERT INTO virtual_users (domain_id, email, password) VALUES 
-      ((SELECT id FROM virtual_domains WHERE domain = 'juansilvaphoto.com'), 'info@juansilvaphoto.com', 'A1T2J3C42024') 
-    ON CONFLICT DO NOTHING;
-    INSERT INTO virtual_aliases (domain_id, source, destination) VALUES 
-      ((SELECT id FROM virtual_domains WHERE domain = 'juansilvaphoto.com'), 'info@juansilvaphoto.com', 'info') 
-    ON CONFLICT DO NOTHING;
-  "
-
-  psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "$insert_sql"
-
-  if [ $? -eq 0 ]; then
-    log "Initial data inserted successfully."
-  else
-    log "Failed to insert initial data."
-  fi
-}
-
-
-function serviceConf {
-  if [[ ! $HOSTNAME =~ \. ]]; then
-    HOSTNAME="$HOSTNAME.$DOMAIN"
-  fi
-
-  for VARIABLE in $(env | cut -f1 -d=); do
-    VAR=${VARIABLE//:/_}
-    VALUE=${!VAR}
-    sed -i "s|{{ $VAR }}|$VALUE|g" /etc/postfix/*.cf
-  done
-
-  if [ -f /overrides/postfix.cf ]; then
-    while read -r line; do
-      [[ -n "$line" && "$line" != [[:blank:]#]* ]] && postconf -e "$line"
-    done < /overrides/postfix.cf
-    echo "Loaded '/overrides/postfix.cf'"
-  else
-    echo "No extra postfix settings loaded because optional '/overrides/postfix.cf' not provided."
-  fi
-
-  if ls -A /overrides/*.map 1> /dev/null 2>&1; then
-    cp /overrides/*.map /etc/postfix/
-    postmap /etc/postfix/*.map
-    rm /etc/postfix/*.map
-    chown root:root /etc/postfix/*.db
-    chmod 0600 /etc/postfix/*.db
-    echo "Loaded 'map files'"
-  else
-    echo "No extra map files loaded because optional '/overrides/*.map' not provided."
-  fi
-}
-
-function setPermissions {
-  # Ensure directories and files have correct permissions
-  log "Setting permissions for Postfix directories and files..."
-
-  # Set ownership and permissions for Postfix directories
-  chown -R postfix:postfix /var/spool/postfix
-  chmod 750 /var/spool/postfix/private
-  chmod 750 /var/spool/postfix
-
-  # Set ownership and permissions for Postfix configuration files
-  chown -R root:root /etc/postfix
-  chmod 640 /etc/postfix/*.cf
-
-  # Set ownership and permissions for mail directories
-  chown -R postfix:postfix /var/mail/
-  chmod 700 /var/mail/
-
-   # Set ownership and permissions for mail directories
-  chown -R info:info /var/mail/
-  chmod 700 /var/mail/
-
-  # Set permissions for SSL certificates
-  chown root:root /etc/ssl/certs/fullchain.pem /etc/ssl/private/privkey.pem
-  chmod 644 /etc/ssl/certs/fullchain.pem
-  chmod 600 /etc/ssl/private/privkey.pem
-}
-
-function serviceStart {
-  addUserInfo
-  createVirtualTables
-  insertInitialData
-  serviceConf
-  setPermissions
-  log "[ Iniciando Postfix... ]"
-  /usr/sbin/postfix start-fg
-}
-
-export DOMAIN=${DOMAIN:-"juansilvaphoto.com"}
-export HOSTNAME=${HOSTNAME:-"mailpost.juansilvaphoto.com"}
-export MESSAGE_SIZE_LIMIT=${MESSAGE_SIZE_LIMIT:-"50000000"}
-export RELAYNETS=${RELAYNETS:-""}
-export RELAYHOST=${RELAYHOST:-""}
-
-serviceStart &>> /proc/1/fd/1
+echo "Permissions and ownership have been set."
