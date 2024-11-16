66dd2a1e024aa6da9f8cfba4ac4e2057773990cf
smartquailstage <smartquail.stage@gmail.com>
2024-11-16 11:12:05 -0500

    upda

--- qnd30dovecot/start.sh
+++ qnd30dovecot/start.sh
@@ -28,22 +28,7 @@ echo "Setting permissions and ownership for all directories and files under $MAI
 find "$MAIL_DIR" -type d -exec chown $USER:$GROUP {} \; -exec chmod 755 {} \;
 find "$MAIL_DIR" -type f -exec chown $USER:$GROUP {} \; -exec chmod 644 {} \;
 
-# Check if /var/spool/postfix/private/auth exists; create it if needed
-if [ ! -e /var/spool/postfix/private/auth ]; then
-    echo "Creating /var/spool/postfix/private/auth socket"
-    mkdir -p /var/spool/postfix/private
-    touch /var/spool/postfix/private/auth
-    chown dovecot:dovecot /var/spool/postfix/private/auth
-    chmod 0666 /var/spool/postfix/private/auth
-fi
 
-# Check if /var/spool/postfix/private/dovecot-lmtp exists; create it if needed
-if [ ! -e /var/spool/postfix/private/dovecot-lmtp ]; then
-    echo "Creating /var/spool/postfix/private/dovecot-lmtp socket"
-    touch /var/spool/postfix/private/dovecot-lmtp
-    chown dovecot:dovecot /var/spool/postfix/private/dovecot-lmtp
-    chmod 0660 /var/spool/postfix/private/dovecot-lmtp
-fi
 
 # Check if /var/mail/info@juansilvaphoto.com/tmp exists; create if needed
 INFO_DIR="$MAIL_DIR/info@juansilvaphoto.com/tmp"
@@ -58,8 +43,8 @@ fi
 echo "Verification of permissions and ownership:"
 ls -ld "$MAIL_DIR"
 ls -ld "$INFO_DIR"
-ls -ld /var/spool/postfix/private/auth
-ls -ld /var/spool/postfix/private/dovecot-lmtp
+
+
 
 echo "Permissions and ownership have been set."
 
