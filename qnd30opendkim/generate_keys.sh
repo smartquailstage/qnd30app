fe61c8e923e7cba785c3cc201540b0a920956605
smartquailstage <smartquail.stage@gmail.com>
2024-11-25 12:43:00 -0500

    d

--- qnd30opendkim/generate_keys.sh
+++ qnd30opendkim/generate_keys.sh
@@ -10,28 +10,28 @@ echo "Generando las claves DKIM..."
 opendkim-genkey -s mailpost -d juansilvaphoto.com -b 2048 -v -D /etc/opendkim/keys || { echo "Error: Fallo al generar las claves DKIM"; exit 1; }
 
 # Verificar que las claves hayan sido generadas
-if [ ! -f /etc/opendkim/keys/mailpost.juansilvaphoto.com.private ]; then
-    echo "Error: No se encontró la clave privada mailpost.juansilvaphoto.com.private"
+if [ ! -f /etc/opendkim/keys/mailpost.private ]; then
+    echo "Error: No se encontró la clave privada mailpost.private"
     exit 1
 fi
 
-if [ ! -f /etc/opendkim/keys/mailpost.juansilvaphoto.com.txt ]; then
-    echo "Error: No se encontró la clave pública mailpost.juansilvaphoto.com.txt"
+if [ ! -f /etc/opendkim/keys/mailpost.txt ]; then
+    echo "Error: No se encontró la clave pública mailpost.txt"
     exit 1
 fi
 
 # Cambiar los permisos de las claves generadas
-chmod 600 /etc/opendkim/keys/mailpost.juansilvaphoto.com.private
-chmod 644 /etc/opendkim/keys/mailpost.juansilvaphoto.com.txt
+chmod 600 /etc/opendkim/keys/mailpost.private
+chmod 644 /etc/opendkim/keys/mailpost.txt
 
 # Cambiar la propiedad de la clave pública para el acceso correcto
-chown opendkim:opendkim /etc/opendkim/keys/mailpost.juansilvaphoto.com.txt
+chown opendkim:opendkim /etc/opendkim/keys/mailpost.txt
 
 # Mostrar las claves generadas
 echo "Clave pública DKIM:"
-cat /etc/opendkim/keys/mailpost.juansilvaphoto.com.txt
+cat /etc/opendkim/keys/mailpost.txt
 
 echo "Clave privada DKIM:"
-cat /etc/opendkim/keys/mailpost.juansilvaphoto.com.private
+cat /etc/opendkim/keys/mailpost.private
 
 echo "Las claves DKIM se generaron correctamente."
